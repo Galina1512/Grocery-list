4101b39ba48059fb3b9dfeeea51e1418a28df1a7
unknown <Kostina13@gmail.com>
2022-01-02 15:20:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import image from './shopping.jpg';
+import imageMan from './man.jpg';
+import { GroceryList } from './GroceryList';
+
+function App() {
+  return (
+    <div className='App'>
+        <img src= { image } width= '200px' alt='shop'/>
+        <h1>Grocery List</h1>
+        <GroceryList/>
+          <img src= { imageMan } width= '200px' alt='man'/>
+    </div>
+  );
+}
+
+export default App;
